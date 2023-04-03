8ba8d300fc08f6c723ff300f812621b9d7547547
Ritik kumar <97829073+Ritik-Kumar-2002@users.noreply.github.com>
2023-04-03 15:55:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TableComp from './Component/TableComp.js';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 style={{textAlign:'center'}}> REACT PROJECT </h1>
+        <TableComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/TableComp.js
@@ -0,0 +1,78 @@
+import { TableHead, TableBody, Table, TableRow, TableContainer, TableCell, Paper, Tab, TextField } from "@mui/material";
+import { Grid } from "@mui/material";
+import Info from './information';
+
+const TableComp = () => {
+    // console.log("info is "+ Info);
+    return (
+        <>
+            <TableContainer component={Paper} elevation={5} style={{ width: "90%", margin: 'auto', border: '0px solid black' }}>
+                <Table >
+                    <TableHead>
+                        <TableRow>
+                            <TableCell style={{
+                                fontWeight: 'bold', backgroundColor: 'whitesmoke', width: "300px"
+                            }}> MONTH 1 </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody >
+                        {Info.map((data) => {
+                            // console.log("data is "+ data.name);
+                            return (
+                                <TableRow key={data.ind} >
+                                    
+                                    <TableCell  style={{ fontWeight: 'bold', backgroundColor:'whitesmoke'}}> {data.name} </TableCell>
+                                       <div style={{display:"flex", width:'100%'}}>
+                                        <Grid >
+                                            <TextField type='text' fullWidth />
+                                        </Grid>
+                                        <Grid >
+                                            <TextField type='text' fullWidth />
+                                        </Grid>
+                                        <Grid >
+                                            <TextField type='text' fullWidth />
+                                        </Grid>
+                                        <Grid >
+                                            <TextField type='text' fullWidth />
+                                        </Grid>
+                                        <Grid >
+                                            <TextField type='text' fullWidth />
+                                        </Grid>
+                                        <Grid >
+                                            <TextField type='text' fullWidth />
+                                        </Grid>
+                                        </div>
+                                        {/* <Grid item lg={2} sm={9} md={9} xs={12}>
+                                            <TextField type='text' fullWidth />
+                                        </Grid>
+                                        <Grid item lg={2} sm={9} md={9} xs={12}>
+                                            <TextField type='text' fullWidth />
+                                        </Grid> */}
+                                        
+                                   
+                                </TableRow>
+                            );
+
+                        })}
+                        {/* <TableRow>
+                            <TableCell style={{ fontWeight: 'bold', backgroundColor:'whitesmoke' }}> Head 1 </TableCell>
+                            <TableCell> data 1 </TableCell>
+                            <TableCell> data 2 </TableCell>
+                            <TableCell> data 3 </TableCell>
+                            <TableCell> data 4 </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell style={{ fontWeight: 'bold' }}> Head 1 </TableCell>
+                            <TableCell> data 1 </TableCell>
+                            <TableCell> data 2 </TableCell>
+                            <TableCell> data 3 </TableCell>
+                            <TableCell> data 4 </TableCell>
+                        </TableRow> */}
+
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    );
+}
+export default TableComp;
